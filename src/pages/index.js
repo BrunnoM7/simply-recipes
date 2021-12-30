cd69673a741ee0c766947cb474b1532820a66877
Brunno Marchetti <brunno@MacBook-Pro-de-Sergio.local>
2021-12-30 14:17:10 -0400

    chore(navbar): created simple component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { Link } from 'gatsby';
+import Navbar from "../components/Navbar";
 
 export default function Home() {
-  return <div>
-    <h1>Hello world!</h1>
+  return (
     <div>
-      <Link to='/about'>about</Link>
+      <Navbar />
+      <h1>Hello world!</h1>
+      <div>
+        <Link to='/about'>about</Link>
+      </div>
+      <div>
+        <Link to='/company/history'>company history</Link>
+      </div>
     </div>
-    <div>
-      <Link to='/company/history'>company history</Link>
-    </div>
-    </div>
+  )
 }
